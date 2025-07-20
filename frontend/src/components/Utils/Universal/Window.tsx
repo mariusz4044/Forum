59dca40b269aec918853392f196c0ccfa4962065
mariusz4044 <mariusz4044@gmail.com>
2025-07-20 17:00:21 +0200

    UI: Add dialog show animation

--- frontend/src/components/Utils/Universal/Window.tsx
+++ frontend/src/components/Utils/Universal/Window.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { motion } from "motion/react";
 import { ReactNode } from "react";
 import { X } from "lucide-react";
 
@@ -10,9 +11,16 @@ interface WindowProps {
 
 export default function Window({ children, title }: WindowProps) {
   return (
-    <div className="fixed w-screen h-screen bg-black/80 left-0 top-0 z-30">
-      <div
+    <motion.div
+      className="fixed w-screen h-screen bg-black/80 left-0 top-0 z-30"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+    >
+      <motion.div
         role="dialog"
+        initial={{ y: -30, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.2, ease: "easeOut" }}
         className="
     fixed top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2
     w-sm h-auto bg-[#23233a] z-30 rounded-lg p-5"
@@ -23,7 +31,7 @@ export default function Window({ children, title }: WindowProps) {
         </header>
         <div className="separator"></div>
         <main>{children}</main>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 }
