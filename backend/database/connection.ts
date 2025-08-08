39b42a7bd9462428dcd8b6247b63950f905e1d6d
mariusz4044 <mariusz4044@gmail.com>
2025-08-08 19:00:14 +0200

    change: build scripts & db query stats

--- backend/database/connection.ts
+++ backend/database/connection.ts
@@ -1,2 +1,15 @@
 import { PrismaClient } from "@prisma/client";
-export const prisma = new PrismaClient();
+
+export const prisma = new PrismaClient({
+  log: [
+    { level: "query", emit: "event" },
+    { level: "info", emit: "stdout" },
+    { level: "warn", emit: "stdout" },
+    { level: "error", emit: "stdout" },
+  ],
+});
+
+prisma.$on("query", (e) => {
+  console.log(`→ QUERY: ${e.query} |  ${e.duration}ms`);
+  console.log(`→ PARAMS: ${e.params}`);
+});
