f03a60a2f61d6ad8e27eb4aebfd1753d0dee9abd
mariusz4044 <mariusz4044@gmail.com>
2025-09-04 15:22:44 +0200

    Add: report cooldown

--- backend/controllers/forum/reportPost.ts
+++ backend/controllers/forum/reportPost.ts
@@ -2,12 +2,27 @@ import { Request, Response } from "express";
 import { prisma } from "../../database/connection";
 import { createReportQuery } from "../dbqueries/forum/createReportQuery";
 import { AppError } from "../../utils/AppError";
+import { updateUniqueUser } from "../dbqueries/user/updateUniqueUser";
+
+const reportDelay = parseInt(process.env.REPORT_DELAY_PER_USER!);
 
 export async function reportPost(req: Request, res: Response) {
   const { postId } = req.body;
+  const user = req.user!;
+
+  const nextReportTime = new Date(user.lastReportTs);
+  nextReportTime.setSeconds(nextReportTime.getSeconds() + 60);
+
+  if (nextReportTime.getTime() > Date.now() && user.role !== "ADMIN") {
+    throw new AppError(
+      `Slow down! wait ${reportDelay} before next report!`,
+      null,
+      429,
+    );
+  }
 
   const isReported = await prisma.report.findFirst({
-    where: { postId, createdById: req.user!.id },
+    where: { postId, createdById: user.id },
   });
 
   if (isReported) {
@@ -16,7 +31,14 @@ export async function reportPost(req: Request, res: Response) {
 
   await createReportQuery({
     postId,
-    createdById: req.user!.id,
+    createdById: user.id,
+  });
+
+  await updateUniqueUser({
+    where: { id: user.id },
+    data: {
+      lastReportTs: new Date(),
+    },
   });
 
   return res.status(200).json({
